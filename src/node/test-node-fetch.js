56f9ba9f9e17f9ff04112bef50f8711e29336c49
Divya Rajaneganth <divya.rajaneganth@gmail.com>
2018-01-28 13:17:36 +0530

    removed unused file

--- src/node/test-node-fetch.js
+++
@@ -1,6 +0,0 @@
-var fetch = require('node-fetch');
-
-console.log("full"+(fetch('http://localhost:5000/getListOfCustomers')
-  .then((response) => {console.log('response: '+response); return response.json();})
-  .then((responseJson) => {console.log('responseData: '+JSON.stringify(responseJson)); return responseJson;})
-  .catch(err => {console.log(err)})))
